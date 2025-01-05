eddee95f886028324c94efcf45c118f45b1a8ba4
Pierre Le Gall <pierre@legall.im>
2025-01-05 19:49:22 +0100

    Some code format

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -13,13 +13,13 @@ async function getPosts() {
     if (file && typeof file === "object" && "metadata" in file && slug) {
       const metadata = file.metadata as Omit<Post, "slug">
       const post = { ...metadata, slug } satisfies Post
-      post.published && posts.push(post)
+      if (post.published) posts.push(post)
     }
   }
 
-  posts = posts.sort(
-    (first, second) => new Date(second.date).getTime() - new Date(first.date).getTime()
-  )
+  posts = posts.sort((first, second) => {
+    return new Date(second.date).getTime() - new Date(first.date).getTime()
+  })
 
   return posts
 }
