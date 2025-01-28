cb64e26849f6b4e27487159a4158da9d65972100
Pierre Le Gall <pierre@legall.im>
2025-01-28 14:55:45 +0100

    Remove @tailwindcss/forms package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
 import containerQueries from "@tailwindcss/container-queries"
-import forms from "@tailwindcss/forms"
 import type { Config } from "tailwindcss"
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
   theme: {
     extend: {}
   },
-
-  plugins: [forms, containerQueries]
+  plugins: [containerQueries]
 } satisfies Config
